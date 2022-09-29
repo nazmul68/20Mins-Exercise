83dac23485664400192e00f548bd7eef9b001373
unknown <nazmul sujon>
2022-09-29 23:48:09 +0600

    toast actively completed

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -1,10 +1,21 @@
 import React from "react";
+import Swal from "sweetalert2";
 
 const Toast = () => {
   return (
     <div className="text-center">
-      <button className="btn btn-primary px-5 py-3 mt-4 fw-bold">
-        {" "}
+      <button
+        className="btn btn-primary px-5 py-3 mt-4 fw-bold"
+        onClick={() => {
+          Swal.fire({
+            position: "top-center",
+            icon: "success",
+            title: "Actively Completed",
+            showConfirmButton: false,
+            timer: 2000,
+          });
+        }}
+      >
         Actively Completed
       </button>
     </div>
